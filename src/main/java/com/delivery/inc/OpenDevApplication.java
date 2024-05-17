80c0e5b8fa4a93a6a90c8ef53a539474a43b981c
Ismael Nunes Campos <ismaeldenunes@gmail.com>
2024-05-17 16:13:51 -0300

    initial commit

---
+++ src/main/java/com/delivery/inc/OpenDevApplication.java
@@ -0,0 +1,13 @@
+package com.delivery.inc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenDevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/delivery/inc/OpenDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delivery.inc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenDevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
